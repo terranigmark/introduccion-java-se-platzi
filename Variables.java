885ceec103428a9aabfea2650b4005d96fbd0f38
Hector Vega <linnk99@gmail.com>
2020-11-06 22:34:57 -0500

    Hello World and variable assignation classes added

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        // Declarando una variable
+        int speed;
+        // Asignamos valor a una variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Mulan The Cat";
+        System.out.println(employeeName);
+    }
+}
