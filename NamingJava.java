0305597cdaf1f8c741160fbfe545ac3f9237b6df
Hector Vega <linnk99@gmail.com>
2020-11-06 22:57:40 -0500

    Naming convention class completed

---
+++ NamingJava.java
@@ -0,0 +1,19 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 1234567890;
+        int celPhone = 1234567890;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Spain";
+        String _backgrounColor = "Green";
+
+
+        String currency$ = "MXN";
+        String background_color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+    }
+}
