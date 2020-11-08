65f0abe01d555b027a86b03f64bdcf9338beebb6
Hector Vega <linnk99@gmail.com>
2020-11-07 21:34:03 -0500

    Arrays and nested loops class

--- Arrays.java
+++ Arrays.java
@@ -13,10 +13,18 @@ public class Arrays {
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donu";
 
-        System.out.println(androidVersions[0]);
+        /*for (int i = 0; i < androidVersions.length; i++) {
+            System.out.println(androidVersions[i]);
+        }*/
+
+        for (String androidVersion : androidVersions) {
+            System.out.println(androidVersion);
+        }
+
+        /*System.out.println(androidVersions[0]);
         System.out.println(androidVersions[1]);
         System.out.println(androidVersions[2]);
-        System.out.println(androidVersions[3]);
+        System.out.println(androidVersions[3]);*/
 
         System.out.println();
         System.out.println();
@@ -30,20 +38,46 @@ public class Arrays {
         cities[3][0] = "Mexico";
         cities[3][1] = "CDMX";
 
-        System.out.println(cities[0][0]);
+        /*System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
         System.out.println(cities[1][1]);
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
+
+        /*for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }*/
+
+        for (String[] pair : cities) {
+            for (String name : pair) {
+                System.out.println(name);
+            }
+        }
 
         String[][][][] animals = new String [2][3][2][2];
         animals[1][0][0][1] = "Monkey";
 
         System.out.println();
         System.out.println();
-        System.out.println(animals[1][0][0][1]);
+        // System.out.println(animals[1][0][0][1]);
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 0; j++) {
+                for (int k = 0; k <= 0; k++) {
+                    for (int l = 0; l <= 1; l++) {
+                        System.out.println(animals[i][j][k][l]);
+                    }
+                }
+            }
+        }
     }
+
+    byte i = 1;
+    byte j = 1;
+    byte k = i + j;
 }
